c44523cbc334392068c7c96594880354512cff0c
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-09-15 00:31:28 +0300

    Fix the do create method

--- console.py
+++ console.py
@@ -121,7 +121,17 @@ class HBNBCommand(cmd.Cmd):
         elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        new_instance = HBNBCommand.classes[args[0]]()
+        if len(arg > 1):
+            for i in arg[1:]:
+                param = i.split("=").replace("_", " ")
+                key = i.split("=")[0]
+                val = i.split("=")[1]
+                try:
+                    param = eval(val)
+                except Exception:
+                    pass
+                setattr(new_instance, key, param)
         storage.save()
         print(new_instance.id)
         storage.save()
