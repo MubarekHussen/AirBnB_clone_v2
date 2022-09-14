2ed5ad7d51fe3f633e82478e85ab317404e6c70a
MubarekHussen <mubahussen2014@gmail.com>
2022-09-15 02:36:45 +0300

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,6 +16,12 @@ class FileStorage:
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
