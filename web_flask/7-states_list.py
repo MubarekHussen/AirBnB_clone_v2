b6c07abd48264c4ed7b0d54d08c91a9f67558ac2
MubarekHussen <mubahussen2014@gmail.com>
2022-10-17 00:47:54 +0300

    trying

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,49 @@
 #!/usr/bin/python3
 """
-    Runs a Flask web application on 0.0.0.0:5000
+starts a Flask web application
 """
-from ../models import storage
-from flask import Flask
-from flask import render_template
+
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def states():
-    """ Returns an HTML page of all States sorted by name """
-    states = storage.all("State")
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """ Removes the current SQLAlchemy session. """
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
 
-if __name__ == "__main__":
-    """ Run on 0.0.0.0 """
-    app.run(host='0.0.0.0')
+from flask import Flask, render_template
+from models import *
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
